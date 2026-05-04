5162bd30e6c4206873dd1a2abb71843f6b2cd11e
Patrick McGrath <pmcgrath7@ipsec-10-2-225-202.vpn.gatech.edu>
2026-05-04 14:47:27 -0400

    Getting started

--- annotateNewImages.py
+++ annotateNewImages.py
@@ -26,15 +26,18 @@ for species in all_species:
 
         img = Image.open(im_file)
         img = ImageOps.exif_transpose(img)
-        crop_fd = result_fd[0].boxes[0].xyxy.cpu().numpy()[0]
-        crop_cd = result_cd[0].boxes[0].xyxy.cpu().numpy()[0]
-
-        result_fp =model_fp(img.crop(crop_fd))
-        result_cp =model_cp(img.crop(crop_cd))
+        try:
+            crop_fd = result_fd[0].boxes[0].xyxy.cpu().numpy()[0]
+            crop_cd = result_cd[0].boxes[0].xyxy.cpu().numpy()[0]
+        except:
+            print(im_file + 'bad data')
+            continue
+        result_fp =model_fp(img.crop(crop_fd), verbose = False)
+        result_cp =model_cp(img.crop(crop_cd), verbose = False)
         result_fp[0].save(fm_obj.localProjectDir + 'Outputs/Fish/'+im_file.split('/')[-1])
         result_cp[0].save(fm_obj.localProjectDir + 'Outputs/ColorBox/'+im_file.split('/')[-1])
 
-    pdb.set_trace()
+pdb.set_trace()
     #boxes = result.boxes  # Boxes object for bounding box outputs
     #masks = result.masks  # Masks object for segmentation masks outputs
     #keypoints = result.keypoints  # Keypoints object for pose outputs
