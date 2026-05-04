b32afec382c99b8f9f0e94d8d2c17199d0a85673
Patrick McGrath <pmcgrath7@ipsec-10-2-225-202.vpn.gatech.edu>
2026-05-04 14:45:34 -0400

    Getting started

--- annotateNewImages.py
+++ annotateNewImages.py
@@ -30,8 +30,11 @@ for species in all_species:
         crop_cd = result_cd[0].boxes[0].xyxy.cpu().numpy()[0]
 
         result_fp =model_fp(img.crop(crop_fd))
-        result_cp =model_fp(img.crop(crop_cd))
-        pdb.set_trace()
+        result_cp =model_cp(img.crop(crop_cd))
+        result_fp[0].save(fm_obj.localProjectDir + 'Outputs/Fish/'+im_file.split('/')[-1])
+        result_cp[0].save(fm_obj.localProjectDir + 'Outputs/ColorBox/'+im_file.split('/')[-1])
+
+    pdb.set_trace()
     #boxes = result.boxes  # Boxes object for bounding box outputs
     #masks = result.masks  # Masks object for segmentation masks outputs
     #keypoints = result.keypoints  # Keypoints object for pose outputs
